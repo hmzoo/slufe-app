bef3bda8ab04f5d86521aba2b8f8e93e2f89192e
Ubuntu <ubuntu@sarthe.fr>
2022-12-01 18:18:22 +0100

    ok

--- myred.js
+++ myred.js
@@ -10,42 +10,50 @@ const rankey = () => { return "" + (100000 + Math.floor(Math.random() * 900000))
 
 
 const myred = {
-   newkey:(uid,n = 12)=>{
+   new_key:(uid,n = 12)=>{
     if (n < 0) {return 0}
     const key = rankey();
     return redis.exists("uid_" + key).then(ans => {
         if (ans == 1) {
           return this.newkey(uid,n - 1);
         }else{
+            redis.del("flux_" + key);
+            redis.del("flw_" + key);
             return redis.set("uid_" + key, uid, 'ex', ttl).then(ans =>{return key});
         }
     })
    },
-   checkkey: (key,uid){
+   check_key: (key,uid){
      return redis.get("uid_" + key).then(ans => ans==uid )
    },
-   set_flux:(key,val)=>{
+   set_flux:(uid,key,val)=>{
       return redis.set("flux_" + key, val, 'ex', ttl);
    },
-   get_flux:(key)=>{
+   get_flux:(uid,key)=>{
     return redis.get("flux_" + key);
    },
    add_fwl:(key,qkey)=>{
     return redis.saad("fwl_" + key,qkey);
    },
-   add_fwl:(key,qkey)=>{
-    return redis.saad("flux_" + key,qkey);
-   },
    del_fwl:(key,qkey)=>{
-        return redis.srem("flux_" + key,qkey);
+        return redis.srem("fwl_" + key,qkey);
    },
    all_fwl:(key,qkey)=>{
-    return redis.smembers("flux_" + key);
+    return redis.smembers("fwl_" + key);
    },
+}
+
+const build_json = (key,qkey,)
+
+const mrjson = {
+
+
 
 
 
 
 }
+
+
 
 module.exports = myred;
