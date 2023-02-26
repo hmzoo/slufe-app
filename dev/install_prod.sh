b377ad5d203182ee6c091e2d2d9ab1bc27fa275c
HMJ <hmj@goatpage.com>
2023-02-26 08:46:06 +0100

    ok

--- dev/install_prod.sh
+++ dev/install_prod.sh
@@ -20,7 +20,17 @@ curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 sudo apt -y install nodejs
 
-#CONTAINERS
-apt-get remove -y redis-server
-sudo pkill traefik
 
+#GIT
+sudo     git config --global user.name HMJ
+sudo     git config --global user.email "hmj@goatpage.com"
+
+#SECURE
+sudo apt-get -y install git fail2ban
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw enable
+
+# git clone
+# docker-compose -f docker-compose.yml up -d --no-deps --build
